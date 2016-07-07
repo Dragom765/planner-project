c67e978631110c91619499d37ff40ede0068a3a8
Dragom765 <01dragom@gmail.com>
2016-07-07 16:32:24 -0400

    Creates the basic html page
    makes a basic layout of the concept for the website

---
+++ webpage/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+  //object for easy manipulation of title, description, and day of week of task
+  var task = {
+    title: '',
+    description: '',
+    weekday: 0
+  }
+  
+  //initiation of the webpage's first screen
+  $("#password").hide();
+  $("#sign-up").hide();
+  $("#option-help").hide();
+  //$("#week-scheduler").hide();
+  $("#email").val("");
+  $("#pswd").val("");
+  
+/* lgin-user-01, plan-head-01 */
+  $("#log-email").click(eValidate);
+  
+/* lgin-pswd-01 */
+  $("#log-pswd").click(pswdValidate);
+  
+/* lgin-npwd-01 */
+  $("#log-create").click(signupValidate);
+  
+/* plan-head-02-03 */
+  $("#help").click(helpText);
+  
+  $(".wkday").click(function() { setDay($(this).text(), task); });
+});
+
+/* lgin-user-01 */
+function eValidate() {
+    if($("#email").val() != "" && $("#email").val() != undefined){
+      $(".signin").toggle();
+      custTitle();
+      }else
+      alert("Please enter your email address.");
+}
+
+/* lgin-pswd-01 */
+var pswdValidate = function() {
+    if($("#pswd").val() != "" && $("#pswd").val() != undefined){
+      $(".top").hide();
+      $("#week-scheduler").show();
+    }else{
+      alert("Please enter your password.");
+    }
+}
+
+/* lgin-npwd-01 */
+var signupValidate = function() {
+    if($("#pswd-create").val() == ""){
+      alert("Please make sure you have entered a password.");
+    }else if($("#pswd-create").val() != $("#confirm").val()){
+      alert("Please make sure your passwords match");
+    }else{
+      $(".top").hide();
+      $("#week-scheduler").show();  
+    }
+}
+
+/* plan-head-01 */
+var custTitle = function() {
+  $("#header").text($("#email").val()+"'s Tasklist");
+}
+
+/* plan-head-02-03 */
+var helpText = function(task) {
+  $("#option-help").toggle();
+}
+
+var setDay = function(txt, task) {
+  $("#wkday-select").text(txt+" ").append("<span class=\"caret\"></span>");
+  
+  if(txt == "Sunday"){
+    task.weekday = 7;
+  }else if(txt == "Monday"){
+    task.weekday = 1;
+  }else if(txt == "Tuesday"){
+    task.weekday = 2;
+  }else if(txt == "Wednesday"){
+    task.weekday = 3;
+  }else if(txt == "Thursday"){
+    task.weekday = 4;
+  }else if(txt == "Friday"){
+    task.weekday = 5;
+  }else if(txt == "Saturday"){
+    task.weekday = 6;
+  }else 
+    alert("Background error. Unable to set day of the week.");
+}
+
+
