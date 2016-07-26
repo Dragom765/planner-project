f3ef690c3724b434f76a59e86a5561761d2f1055
Dragom765 <01dragom@gmail.com>
2016-07-26 15:44:46 -0400

    Don't want to log the user's email

--- app/mysql/delete.js
+++ app/mysql/delete.js
@@ -4,8 +4,6 @@ module.exports = {
     var id = req.params.id;
     var email = req.params.email;
     
-    console.log(email+"\n"+id)
-    
     con.query("DELETE FROM tasks WHERE id = ? AND email = ?", [id, email], function(err) {
       if(err)
         res.send(err.message);
